feec96bb7c92a3c3b966292f78ee06110b4b2b39
Beyzanur <90965586+beyzanurylmaz@users.noreply.github.com>
2023-04-19 18:20:19 +0300

    Add files via upload

---
+++ cs_terstenYazdir/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string isim;
+Console.Write("Bir isim giriniz : ");
+
+isim = Console.ReadLine()!;
+
+for (int i = isim.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(isim[i]);
+}
+
